2b48f47c552b107e2b94f6f57c881721ad7cfb7e
jaketrent <trent.jake@gmail.com>
2016-05-30 11:24:54 -0600

    initial import

---
+++ index.js
@@ -0,0 +1,10 @@
+const koa = require('koa')
+const static = require('koa-static')
+
+const app = koa()
+const port = process.env.PORT || 3000
+
+app.use(static(__dirname))
+
+app.listen(port)
+console.log(`Listening on port ${port}`)
